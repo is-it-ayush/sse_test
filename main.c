b224b9dc5a13611ca525929d3af78c862dc07008
Ayush Gupta <ayush.keshav2004@gmail.com>
2024-09-08 15:01:28 +0530

    chore: clean code

--- main.c
+++ main.c
@@ -1,31 +1,41 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <xmmintrin.h>
 #include <x86intrin.h>
+#include <xmmintrin.h>
 
-int main() {
+#ifndef ITERATION_COUNT
+#define ITERATION_COUNT 100000000;
+#endif
+
+uint64_t average_cycle_taken(void operation()) {
+  int iteration = ITERATION_COUNT;
+  uint64_t start = __rdtsc();
+  for (int i = 0; i <= iteration; ++i) {
+    operation();
+  }
+  uint64_t end = __rdtsc();
+  return ((end - start) / iteration);
+}
+
+void packed_add() {
   float *data = aligned_alloc(16, 16);
   data[0] = 1.0;
   data[1] = 2.0;
   data[2] = 3.0;
   data[3] = 4.0;
 
-  int iteration = 100000000;
-  uint64_t start = __rdtsc();
-  for (int i = 0; i <= iteration; ++i) {
-    __m128 a = _mm_load_ps(data);
-    __m128 b = _mm_load_ps(data);
-    __m128 s = _mm_add_ps(a, b);
-  }
-  uint64_t end = __rdtsc();
+  __m128 s = _mm_add_ps(_mm_load_ps(data), _mm_load_ps(data));
 
-  // float r[4];
-  // _mm_store_ps(r, s);
-  //
-  printf("Clock Cycles: %lu \n", ((end - start) / iteration));
-  // printf("%f %f %f %f", r[0], r[1], r[2], r[3]);
+  float r[4];
+  _mm_store_ps(r, s);
 
   free(data);
+}
+
+int main() {
+  uint64_t cycles = average_cycle_taken(&packed_add);
+
+  printf("Clock Cycles: %ld \n", cycles);
   return 0;
 }
